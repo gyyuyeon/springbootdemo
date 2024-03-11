24fac8a9d2eaff2ff0e0c6425d63fa70fcbba3a1
gyyuyeon <gyyuyeon@gmail.com>
2024-03-11 17:57:34 +0900

    new

---
+++ springboot-workspace/spring02_todolist_maven-1/src/main/java/com/company/Spring02TodolistMaven1Application.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring02TodolistMaven1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring02TodolistMaven1Application.class, args);
+	}
+
+}
--- springboot-workspace/spring02_todolist_maven/src/main/java/com/company/todo/repository/TodoRepository.java
+++ springboot-workspace/spring02_todolist_maven/src/main/java/com/company/todo/repository/TodoRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.company.todo.dto.TodoDTO;
 
@@ -14,7 +15,9 @@ import com.company.todo.dto.TodoDTO;
 수정   /todo/{id}/{completed}    PUT
 삭제   /todo/{id}                DELETE
 */
-
+// 다른 도메인에 대하여 접근허용
+//@CrossOrigin(origins = {"http://127.0.0.1:3000"})
+@CrossOrigin("*")
 @Mapper
 @Repository  // 어노테이션 이걸로 설정하면 알아서 설정됨  - db와 연결
 //- @Mapper 랑연결 해야됨대신(xml메퍼에 주소작성과 @Mapper로연결)
