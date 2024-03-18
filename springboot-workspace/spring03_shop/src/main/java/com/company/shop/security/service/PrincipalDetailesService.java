94b94a6d8de258fa242f32aaedae0e050e88568c
gyyuyeon <gyyuyeon@gmail.com>
2024-03-18 18:22:57 +0900

    new

---
+++ springboot-workspace/spring03_shop/src/main/java/com/company/shop/security/service/PrincipalDetailesService.java
@@ -0,0 +1,39 @@
+package com.company.shop.security.service;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.company.shop.members.dto.MembersDTO;
+import com.company.shop.members.repository.MembersRepository;
+
+import lombok.extern.slf4j.Slf4j;
+@Slf4j
+@Service
+public class PrincipalDetailesService implements UserDetailsService {
+	@Autowired
+	private MembersRepository membersRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String memberEmail) throws UsernameNotFoundException {
+		log.info("PrincipalService username:{}",memberEmail);
+		
+		MembersDTO membersDTO = membersRepository.selectByEmail(memberEmail);
+		log.info("memberEmail:{} memberPass:{} memberName:{}",
+				membersDTO.getMemberEmail(),membersDTO.getMemberPass(),
+				membersDTO.getMemberName());
+		
+		if(membersDTO == null) {
+			throw new UsernameNotFoundException(memberEmail);
+		}
+		return null;
+	}
+
+	
+
+}
